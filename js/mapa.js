06ceb4da0f6eceb0977b9228e467eb8397e79859
JoaoTomas <joaotomas107@gmail.com>
2015-11-26 14:56:17 +0000

    Update mapa.js

--- js/mapa.js
+++ js/mapa.js
@@ -15,15 +15,15 @@ $(document).ready(function(){
           var estacao = $(this).attr('name');
           if (number%2 != 0)    //se count mouseclicks em cada <g> impar escreve na primeira
           {
-              if($("#destino").val() != estacao)
-                  $("#origem").val(estacao);
+              if($("#select2").val() != estacao)
+                  $("#select1").val(estacao);
               else
                   number--;
           }
           else  //se count mouseclicks em cada <g> par escreve na segunda
           {
-              if($("#origem").val() != estacao)
-                  $("#destino").val(estacao);
+              if($("#select1").val() != estacao)
+                  $("#select2").val(estacao);
               else
                   number--;
           }
@@ -31,7 +31,32 @@ $(document).ready(function(){
     
     $('.menu').click(function(){
         number=0;
-        $("#origem").val(null);
-        $("#destino").val(null);
-    });     
+        $("#select1").val(null);
+        $("#select2").val(null);
+    });
+    
+  //Nao permitir que se selecione a mesma estacao 2x
+  function preventDupes( select, index ) {
+      var options = select.options,
+      len = options.length;
+      while( len-- ) {
+          options[ len ].disabled = false;
+      }
+      select.options[ index ].disabled = true;
+      if( index === select.selectedIndex ) {
+          //alert('You\'ve already selected the item "' + select.options[index].text + '".\n\nPlease choose another.');
+          this.selectedIndex = 0;
+      }
+  }
+
+  var select1 = select = document.getElementById( 'select1' );
+  var select2 = select = document.getElementById( 'select2' );
+
+  select1.onchange = function() {
+      preventDupes.call(this, select2, this.selectedIndex );
+  };
+
+  select2.onchange = function() {
+      preventDupes.call(this, select1, this.selectedIndex );
+  };
 });
